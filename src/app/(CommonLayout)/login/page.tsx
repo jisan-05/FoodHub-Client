babc52617d9bf1502ade3af804edfbcc61755d10
jisan-05 <zishantanvir05@gmail.com>
2026-02-03 19:21:27 +0600

    add google login

--- src/app/(CommonLayout)/login/page.tsx
+++ src/app/(CommonLayout)/login/page.tsx
@@ -1,9 +1,10 @@
+import { LoginForm } from '@/components/modules/authentication/login-form';
 import React from 'react';
 
 const LoginPage = () => {
     return (
         <div>
-            
+            <LoginForm></LoginForm>
         </div>
     );
 };
--- src/app/(CommonLayout)/register/page.tsx
+++ src/app/(CommonLayout)/register/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { SignupForm } from "@/components/modules/authentication/signup-form";
+import { authClient } from "@/lib/auth-client";
+import { CloudCog } from "lucide-react";
 
-const RegisterPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const RegisterPage = async() => {
+
+    const session = await authClient.getSession()
+
+    console.log(session)
+
+  return (
+    <div>
+      <SignupForm></SignupForm>
+    </div>
+  );
 };
 
 export default RegisterPage;
